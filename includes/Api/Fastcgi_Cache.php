55a355766d58b31a13589f9dc5ba74ab9d7368da
Al-Amin Firdows <alaminfirdows@gmail.com>
2024-01-23 15:36:04 +0600

    fix: run phpcbf

--- includes/Api/Fastcgi_Cache.php
+++ includes/Api/Fastcgi_Cache.php
@@ -8,7 +8,7 @@ class Fastcgi_Cache {
      * API constructor.
      */
     public function __construct() {
-        flywp()->router->get( 'fastcgi-cache', [$this, 'handle_cache_setting'] );
+        flywp()->router->get( 'fastcgi-cache', [ $this, 'handle_cache_setting' ] );
     }
 
     /**
@@ -17,13 +17,15 @@ class Fastcgi_Cache {
      * @return void
      */
     public function handle_cache_setting( $args ) {
-        if ( !isset( $args['cache'] ) ) {
-            wp_send_json_error( [
-                'message' => 'Missing cache.',
-            ] );
+        if ( ! isset( $args['cache'] ) ) {
+            wp_send_json_error(
+                [
+					'message' => 'Missing cache.',
+				]
+            );
         }
 
-        $valid_types = ['fastcgi', 'none'];
+        $valid_types = [ 'fastcgi', 'none' ];
         $cache       = isset( $args['cache'] ) && in_array( $args['cache'], $valid_types ) ? $args['cache'] : 'none';
 
         $settings            = flywp()->fastcgi->settings();
@@ -31,8 +33,10 @@ class Fastcgi_Cache {
 
         update_option( flywp()->fastcgi::SETTINGS_KEY, $settings );
 
-        wp_send_json_success( [
-            'message' => 'Updated successfully.',
-        ] );
+        wp_send_json_success(
+            [
+				'message' => 'Updated successfully.',
+			]
+        );
     }
 }
