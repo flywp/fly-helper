62f1f9fda5c13dc715e3d69e332b586fc5e330ab
Al-Amin Firdows <alaminfirdows@gmail.com>
2024-01-23 16:42:11 +0600

    fix: cache toggle api (#3)
    Fixes flywp/flywp-app#144

--- includes/Api/Fastcgi_Cache.php
+++ includes/Api/Fastcgi_Cache.php
@@ -8,7 +8,7 @@ class Fastcgi_Cache {
      * API constructor.
      */
     public function __construct() {
-        flywp()->router->post( 'fastcgi-cache', [$this, 'handle_cache_setting'] );
+        flywp()->router->post( 'fastcgi-cache', [ $this, 'handle_cache_setting' ] );
     }
 
     /**
@@ -17,22 +17,25 @@ class Fastcgi_Cache {
      * @return void
      */
     public function handle_cache_setting( $args ) {
-        if ( !isset( $args['status'] ) ) {
-            wp_send_json_error( [
-                'message' => 'Missing status.',
-            ] );
+        if ( ! isset( $args['status'] ) ) {
+            wp_send_json_error(
+                [
+					'message' => 'Missing status.',
+				]
+            );
         }
 
-        $valid_types = ['enable', 'disable'];
-        $type        = isset( $_GET['type'] ) && in_array( $_GET['type'], $valid_types ) ? $_GET['type'] : 'enable';
+        $enabled = isset( $args['status'] ) && $args['status'] === 'enabled' ? true : false;
 
         $settings            = flywp()->fastcgi->settings();
-        $settings['enabled'] = $type === 'enable' ? true : false;
+        $settings['enabled'] = $enabled;
 
         update_option( flywp()->fastcgi::SETTINGS_KEY, $settings );
 
-        wp_send_json_success( [
-            'message' => 'Updated successfully.',
-        ] );
+        wp_send_json_success(
+            [
+				'message' => 'Updated successfully.',
+			]
+        );
     }
 }
